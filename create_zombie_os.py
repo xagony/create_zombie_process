531deb9a3aa25ddbae52efc9b518aec9c27f0934
ogorokhovskyi <Oleksandr.Gorokhovskyi@gamma.co.uk>
2018-09-25 04:50:25 +0100

    looks tidier

--- create_zombie_os.py
+++ create_zombie_os.py
@@ -3,17 +3,12 @@ import os, sys, time
 
 ttl=60
 
-# Fork the process
 pid = os.fork()
-print(pid)
+
 if pid == 0:
- # This is the child process
- # Going zombie
   time.sleep(ttl)
   sys.exit(0)
 else:
-  # This is the parent process 
-  # kill finished process by reading its exit code
   os.wait() 
     
 
